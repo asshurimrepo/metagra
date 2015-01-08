6fe960c85abe74dda9e6b56d9767184be51b2b09
asshurimjoylarita <asshurim@cjnetsolutions.com>
2015-01-08 17:26:09 +0800

    Migrating Theme

--- page-expertise.php
+++ page-expertise.php
@@ -13,7 +13,7 @@
 	<section class="container featured-products">
 
 		<article class="row list">
-			<figure class="col-md-5"><img src="images/market.jpg" alt="#" /></figure>
+			<figure class="col-md-5"><img src="<?=asset('images/market.jpg')?>" alt="#" /></figure>
 			<div class="summary col-md-7">
 				<h1 class="hidden-xs hidden-sm">Markets</h1>
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
