eabf1fcffe305717fe135c05d6f433f265317112
asshurimjoylarita <asshurim@cjnetsolutions.com>
2015-01-08 17:45:31 +0800

    Migrating Theme

--- header.php
+++ header.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<!--[if IE 7]>
-<html class="ie ie7" <?php language_attributes(); ?>>
-<![endif]-->
-<!--[if IE 8]>
-<html class="ie ie8" <?php language_attributes(); ?>>
-<![endif]-->
-<!--[if !(IE 7) | !(IE 8) ]><!-->
-<html <?php language_attributes(); ?>>
-
-<!--<![endif]-->
+<html>
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
