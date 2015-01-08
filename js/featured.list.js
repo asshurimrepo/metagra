4c4aeb1a00cd9c8b0986c95dd5ffb3fef2b3a21c
asshurimjoylarita <asshurim@cjnetsolutions.com>
2015-01-08 15:37:33 +0800

    Initial commit

---
+++ js/featured.list.js
@@ -0,0 +1,22 @@
+(function(){
+
+
+
+    $("#featured-list").owlCarousel({
+
+        autoPlay: 3000,
+        items : 4
+
+    });
+
+    $(".list-5").owlCarousel({
+
+        autoPlay: 3000,
+        items : 5
+
+    });
+
+
+
+
+})();
