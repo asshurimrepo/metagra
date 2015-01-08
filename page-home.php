fd7d81566c25e85a2e9d0c1583f6c41b773ae8a5
asshurimjoylarita <asshurim@cjnetsolutions.com>
2015-01-08 20:03:29 +0800

    Migrating Theme

--- page-home.php
+++ page-home.php
@@ -7,7 +7,6 @@
 
 
 	$posts = get_field('posts');
-	var_dump($posts);
 ?>
 
 <!--Section: Spotlight-->
@@ -20,30 +19,23 @@
 
 <section id="blog" class="container">
 
-	<h2 class="no-background"><span>Who we are?</span></h2>
+	<?php foreach($posts as $post): ?>
+
+
+	<h2 class="no-background"><span><?=$post->post_title?></span></h2>
 
 	<article class="row list">
 		<figure class="col-md-5"><img src="<?=asset('images/post-1.jpg')?>" alt="#" /></figure>
-		<div class="summary col-md-7">We are a Dubai based subsidiary of Middle East Grain Company.
-			The group is headquartered in Egypt since 1992 and trading in a wide varieties of commodities in Egypt and MENA (Middle East and North Africa) region.
-
+		<div class="summary col-md-7">
+			<?=$post->post_excerpt?>
 			<div class="controls">
 				<a class="btn btn-danger" href="#">READ MORE</a>
 			</div>
 		</div>
 	</article>
 
-	<h2 class="no-background"><span>What we do?</span></h2>
+	<?php endforeach; ?>
 
-	<article class="row list">
-		<figure class="col-md-5"><img src="<?=asset('images/post-1.jpg')?>" alt="#" /></figure>
-		<div class="summary col-md-7">The company’s main business activities include international trade, the import and export of bulk commodities. We have direct access to some of the world’s biggest suppliers of bulk commodities, who have the capability to deliver products to  qualified  clients on time and as commited.We are dealing with various commodities in Grain, Metals, Energy and Investment projects.
-
-			<div class="controls">
-				<a class="btn btn-danger" href="#">READ MORE</a>
-			</div>
-		</div>
-	</article>
 
 
 </section>
