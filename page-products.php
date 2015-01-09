ae44ec12037934c0580bc8d7d01673ba6a5e39c5
asshurimjoylarita <asshurim@cjnetsolutions.com>
2015-01-09 21:43:35 +0800

    Migrating Theme

--- page-products.php
+++ page-products.php
@@ -58,7 +58,6 @@
 
 	<section class="container">
 		<h2 class="no-background void-top"><span>Agriculture</span></h2>
-		<p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
 		<div class="space-md"></div>
 
