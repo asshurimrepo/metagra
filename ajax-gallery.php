478a8961432103b24f1442bd38fae9f6f5b5a8a6
asshurimjoylarita <asshurim@cjnetsolutions.com>
2015-01-10 02:23:02 +0800

    Migrating Theme

--- ajax-gallery.php
+++ ajax-gallery.php
@@ -2,7 +2,7 @@
 
 <div class="summary col-md-12">
 	<div class="featured-list gallery">
-
+		<h1><?=$post->post_title?></h1>
 	<?php foreach($gallery as $i => $item): ?>
 			<div class="text-center col-sm-3"> <figure style="background-image: url(<?=$item['image']?>)"><a href="#"><img src="<?=asset('images/magnify.png')?>" alt="#"/></a></figure> <h3><?=$item['text']?></h3>  </div>
 			<?php if($i%4 == 3): ?>
