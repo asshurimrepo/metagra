78e13616c15146ea6eafb48b979ee55fbb5432f2
asshurimjoylarita <asshurim@cjnetsolutions.com>
2015-01-10 02:08:45 +0800

    Migrating Theme

--- page-expertise.php
+++ page-expertise.php
@@ -36,7 +36,7 @@
 
 	<div class="space-lg"></div>
 
-	<section class="container">
+	<div class="container">
 		<h2 class="no-background void-top"><span>Our Partners</span></h2>
 		<p class="text-center spacious">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 		<div class="space-md"></div>
@@ -51,7 +51,7 @@
 			</div>
 
 		</figure>
-	</section>
+	</div>
 
 
 <?php get_footer();
