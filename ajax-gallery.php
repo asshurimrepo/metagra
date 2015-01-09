c36300e4051d7240cfc2ce8119c462031bf69cf4
asshurimjoylarita <asshurim@cjnetsolutions.com>
2015-01-09 17:26:24 +0800

    Migrating Theme

---
+++ ajax-gallery.php
@@ -0,0 +1,4 @@
+<figure class="col-md-5"><?=get_the_post_thumbnail($post->ID)?></figure>
+<div class="summary col-md-7">
+	Gallery
+</div>
--- inc/posts_action.php
+++ inc/posts_action.php
@@ -3,6 +3,7 @@
 	function get_post_content($params){
 		$post_id = $params['id'];
 		$post = get_post($post_id);
+		$template = get_field('tempalte', $post_id);
 
-		include( locate_template('ajax-default.php') );
+		include( locate_template($template) );
 	}
