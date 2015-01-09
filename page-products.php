469cda22f8907304df3802fba078827713546a04
asshurimjoylarita <asshurim@cjnetsolutions.com>
2015-01-09 22:05:40 +0800

    Migrating Theme

--- page-products.php
+++ page-products.php
@@ -64,7 +64,7 @@
 
 		<div class="featured-list gallery">
 			<?php foreach($featured_product_gallery as $i=>$item): ?>
-				<div class="text-center col-sm-3"> <figure style="background-image: url(<?=$item['image']?>)"><a href="#"><img src="<?=asset('images/magnify.png')?>" alt="#"/></a></figure> <h3><?=$item['text']?></h3>  </div>
+				<div class="text-center col-sm-3 item"> <figure style="background-image: url(<?=$item['image']?>)"><a href="#"><img src="<?=asset('images/magnify.png')?>" alt="#"/></a></figure> <h3><?=$item['text']?></h3>  </div>
 				<?php if($i%4 == 3): ?>
 					<div class="space-md col-xs-12"></div>
 				<?php endif; ?>
