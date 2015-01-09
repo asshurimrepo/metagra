d2eb47389a009f8c6608a9e46138f7115a82340f
asshurimjoylarita <asshurim@cjnetsolutions.com>
2015-01-09 17:10:02 +0800

    Migrating Theme

--- ajax-default.php
+++ ajax-default.php
@@ -1,4 +1,3 @@
-<?php global $post; ?>
 <figure class="col-md-5"><?=get_the_post_thumbnail($post->ID)?></figure>
 <div class="summary col-md-7">
 	<h1 class="hidden-xs hidden-sm"><?=$post->post_title?></h1>
