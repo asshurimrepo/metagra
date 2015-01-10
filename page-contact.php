98ca258eeb7e4945282a83aa370c8afc1f829979
asshurimjoylarita <asshurim@cjnetsolutions.com>
2015-01-11 01:02:55 +0800

    Migrating Theme

--- page-contact.php
+++ page-contact.php
@@ -11,9 +11,7 @@
 	<h2 class="no-background"><span>Contact <b>Us</b></span></h2>
 
 	<section class="map">
-		<iframe
-			src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3613.95494997456!2d55.140971!3d25.069516!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f6cada4c46285%3A0xdc22defd3201061c!2sDubai+Multi+Commodities+Center+Authority!5e0!3m2!1sen!2sph!4v1420902592672"
-			width="100%" height="450" frameborder="0" style="border:0"></iframe>
+		<iframe src="https://mapsengine.google.com/map/u/0/embed?mid=zB5T74wOKau0.kz4bPhrycyBk" width="100%" height="480"></iframe>
 	</section>
 
 	<div class="space-md"></div>
