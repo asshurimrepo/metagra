a4022d642d2043a0592a6fd178668155c2070b5d
asshurimjoylarita <asshurim@cjnetsolutions.com>
2015-01-11 00:57:23 +0800

    Migrating Theme

--- page-contact.php
+++ page-contact.php
@@ -5,22 +5,24 @@
 	*/
 
 	get_header();
-	get_template_part('breadcrumbs');
+	get_template_part( 'breadcrumbs' );
 ?>
 
-	<h2 class="no-background"><span>Contact Us</span></h2>
+	<h2 class="no-background"><span>Contact <b>Us</b></span></h2>
 
-<section class="map">
-	<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3613.95494997456!2d55.140971!3d25.069516!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f6cada4c46285%3A0xdc22defd3201061c!2sDubai+Multi+Commodities+Center+Authority!5e0!3m2!1sen!2sph!4v1420902592672" width="100%" height="450" frameborder="0" style="border:0"></iframe>
-</section>
+	<section class="map">
+		<iframe
+			src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3613.95494997456!2d55.140971!3d25.069516!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f6cada4c46285%3A0xdc22defd3201061c!2sDubai+Multi+Commodities+Center+Authority!5e0!3m2!1sen!2sph!4v1420902592672"
+			width="100%" height="450" frameborder="0" style="border:0"></iframe>
+	</section>
 
+	<div class="space-md"></div>
 
 	<div id="contact">
 		<div class="spc-md"></div>
 		<div class="container">
 
 			<div class="col-md-8">
-
 
 
 			</div>
@@ -31,6 +33,7 @@
 				<a href="#" style="color: #ff7200; font-size: 40px; margin:0 6px;"><i class="fa fa-twitter-square"></i></a>
 
 				<p>
+
 				<h3>Email us</h3>
 				<span class="italic lead">info@metagra.ae</span>
 
