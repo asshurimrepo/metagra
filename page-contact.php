3b9641db2c0f8edfa1a0226c8c616f9f247fbb39
asshurimjoylarita <asshurim@cjnetsolutions.com>
2015-01-11 00:55:57 +0800

    Migrating Theme

--- page-contact.php
+++ page-contact.php
@@ -8,9 +8,47 @@
 	get_template_part('breadcrumbs');
 ?>
 
+	<h2 class="no-background"><span>Contact Us</span></h2>
+
 <section class="map">
 	<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3613.95494997456!2d55.140971!3d25.069516!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f6cada4c46285%3A0xdc22defd3201061c!2sDubai+Multi+Commodities+Center+Authority!5e0!3m2!1sen!2sph!4v1420902592672" width="100%" height="450" frameborder="0" style="border:0"></iframe>
 </section>
+
+
+	<div id="contact">
+		<div class="spc-md"></div>
+		<div class="container">
+
+			<div class="col-md-8">
+
+
+
+			</div>
+
+			<div class="col-md-4 text-right">
+
+				<a href="#" style="color: #ff7200; font-size: 40px; margin:0 6px;"><i class="fa fa-facebook-square"></i></a>
+				<a href="#" style="color: #ff7200; font-size: 40px; margin:0 6px;"><i class="fa fa-twitter-square"></i></a>
+
+				<p>
+				<h3>Email us</h3>
+				<span class="italic lead">info@metagra.ae</span>
+
+				<h3>Call us</h3>
+				<span class="italic lead">+971 4369 9787</span>
+
+				<h3>Address</h3>
+				<span class="italic lead">Office 1402</span>
+				<span class="italic lead">Palatinium Tower, JLT</span>
+				<span class="italic lead">Dubai, UAE</span>
+
+				</p>
+			</div>
+		</div>
+
+		<div class="spc-lg"></div>
+
+	</div>
 
 
 <? get_footer();
