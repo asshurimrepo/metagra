c1c53a9f81cd576917a1ee4e87c335593b5925e5
Admin <powerlogic1992@gmail.com>
2015-01-18 16:02:13 +0800

    This is my first commit.

--- header.php
+++ header.php
@@ -79,7 +79,7 @@
 
 					</ul>
 					<div class="col-sm-3 col-md-3 pull-right">
-						<form class="navbar-form" role="search" id="nav-search-form">
+						<form class="navbar-form" role="search">
 							<div class="input-group input-group-sm">
 								<input type="text" class="form-control" placeholder="Search" name="q">
 								<div class="input-group-btn">
