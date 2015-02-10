c38d043266dd20743d235b8ce02bf54a1e456084
Admin <powerlogic1992@gmail.com>
2015-02-10 21:51:10 +0800

    contact page edited.

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	$urlArray = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 	$segments = explode('/', $urlArray);
 	$numSegments = count($segments);
-	$currentSegment = $segments[$numSegments - 1];
+	$currentSegment = $segments[$numSegments - 2];
 
 	echo $currentSegment;
 	?>
