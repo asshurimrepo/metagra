23da002db4c6a57505e37dbaddb365ff2ff842d6
Admin <powerlogic1992@gmail.com>
2015-02-10 21:48:08 +0800

    contact page edited.

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php get_header(); get_template_part('breadcrumbs');  have_posts(); the_post(); ?>
 
+	<?php
+	$urlArray = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+	$segments = explode('/', $urlArray);
+	$numSegments = count($segments);
+	$currentSegment = $segments[$numSegments - 1];
+
+	echo $currentSegment;
+	?>
+
 	<section class="container">
 
 		<article class="row list box no-pad-sides">
