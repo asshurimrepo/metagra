caff230a763cf0bc93503e3a05b612b32e093a5b
Admin <powerlogic1992@gmail.com>
2015-02-11 11:11:11 +0800

    contact page edited.

--- search.php
+++ search.php
@@ -11,36 +11,39 @@ get_header(); ?>
 
 	<section class="container">
 		<div class="space-lg"></div>
-		<div class="box" style="padding-left: 15px; padding-right: 15px;">
+		<div class="box" >
 			<h2 class="no-background void-top full-width"><span><?php printf( __( '<b>Search Results for</b>: %s', 'twentyfourteen' ), get_search_query() ); ?></h2>
-			<div class="row list no-pad-sides">
-				<div id="content" class="site-content" role="main">
+			<div class="space-md">
+			<div style="padding-left: 15px; padding-right: 15px;">
+				<div class="row list no-pad-sides" >
+					<div id="content" class="site-content" role="main">
 
-					<?php if ( have_posts() ) : ?>
+						<?php if ( have_posts() ) : ?>
 
-						<div class="space-sm">
+
+							<?php
+							// Start the Loop.
+							while ( have_posts() ) : the_post();
+
+								/*
+								 * Include the post format-specific template for the content. If you want to
+								 * use this in a child theme, then include a file called called content-___.php
+								 * (where ___ is the post format) and that will be used instead.
+								 */
+								get_template_part( 'content', get_post_format() );
+
+							endwhile;
+						else :
+							// If no content, include the "No posts found" template.
+							?>
+							<div class="page-header">
+								<h1 class="page-title">No Results Found</h1>
+							</div>
 						<?php
-						// Start the Loop.
-						while ( have_posts() ) : the_post();
-
-							/*
-                             * Include the post format-specific template for the content. If you want to
-                             * use this in a child theme, then include a file called called content-___.php
-                             * (where ___ is the post format) and that will be used instead.
-                             */
-							get_template_part( 'content', get_post_format() );
-
-						endwhile;
-					else :
-						// If no content, include the "No posts found" template.
+						endif;
 						?>
-						<div class="page-header">
-							<h1 class="page-title">No Results Found</h1>
-						</div>
-					<?php
-					endif;
-					?>
-				</div><!-- #content -->
+					</div><!-- #content -->
+				</div>
 			</div>
 		</div>
 
