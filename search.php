4c7e7941ca92fd8cb690c0cb5882195b12773fd7
Admin <powerlogic1992@gmail.com>
2015-02-11 13:01:23 +0800

    contact page edited.

--- search.php
+++ search.php
@@ -32,7 +32,7 @@ get_header(); ?>
 							 * use this in a child theme, then include a file called called content-___.php
 							 * (where ___ is the post format) and that will be used instead.
 							 */
-							get_template_part( 'content', get_post_format() );
+							get_template_part( 'search_content', get_post_format() );
 							echo '<div class="space-md"></div>';
 						endwhile;
 					else :
