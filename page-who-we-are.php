fb110eb39ae88773a3fbbd50bf4d4c23e29d9961
Admin <powerlogic1992@gmail.com>
2015-02-12 15:51:44 +0800

    contact page edited.

--- page-who-we-are.php
+++ page-who-we-are.php
@@ -8,45 +8,46 @@ get_header(); get_template_part('breadcrumbs');  have_posts(); the_post();
 $args = [
     'include' => ['6', '330'],
 ];
-$pages = get_pages( $args );
-
-trace($pages);
-?>
+$pages = get_pages( $args ); ?>
 
 <section class="container">
     <div class="box" style="padding-left: 15px; padding-right: 15px;">
-        <article class="row list no-pad-sides">
-            <figure class="col-md-5">
-                <?php if(has_post_thumbnail(get_the_ID())): ?>
-                    <?php echo get_the_post_thumbnail(get_the_ID()); ?>
-                <?php else: ?>
-                    <img src="<?=asset('images/image-empty.jpg') ?>" class="img-responsive" alt="Image Empty"/>
-                <?php endif; ?>
-            </figure>
 
-            <div class="summary col-md-7">
-                <h1 class="hidden-xs hidden-sm">
-                    <?php
-                    $title = get_the_title();
-                    if($currentSegment == 'who-we-are'){
-                        $keyword = "Who";
-                        echo '<span style="font-weight: normal; text-transform: capitalize;">'.str_ireplace($keyword, '<span class="emp-orange">'.$keyword.'</span>', $title).'</span>';
-                    }else if($currentSegment == 'what-we-do'){
-                        $keyword = "What";
-                        echo '<span style="font-weight: normal; text-transform: capitalize;">'.str_ireplace($keyword, '<span class="emp-orange">'.$keyword.'</span>', $title).'</span>';
-                    }else
-                    {
-                        the_title();
-                    }
-                    ?>
-                </h1>
+        <?php foreach( $pages as $page ) : ?>
+            <article class="row list no-pad-sides">
+                <figure class="col-md-5">
+                    <?php if(has_post_thumbnail( $page->ID )): ?>
+                        <?php echo get_the_post_thumbnail( $page->ID ); ?>
+                    <?php else: ?>
+                        <img src="<?=asset('images/image-empty.jpg') ?>" class="img-responsive" alt="Image Empty"/>
+                    <?php endif; ?>
+                </figure>
 
-                <div class="space-sm">
-                    <?php the_content(); ?>
+                <div class="summary col-md-7">
+                    <h1 class="hidden-xs hidden-sm">
+                        <?php
+                        $title = get_the_title( $page->ID );
+                        if($currentSegment == 'who-we-are'){
+                            $keyword = "Who";
+                            echo '<span style="font-weight: normal; text-transform: capitalize;">'.str_ireplace($keyword, '<span class="emp-orange">'.$keyword.'</span>', $title).'</span>';
+                        }else if($currentSegment == 'what-we-do'){
+                            $keyword = "What";
+                            echo '<span style="font-weight: normal; text-transform: capitalize;">'.str_ireplace($keyword, '<span class="emp-orange">'.$keyword.'</span>', $title).'</span>';
+                        }else
+                        {
+                            the_title();
+                        }
+                        ?>
+                    </h1>
+
+                    <div class="space-sm">
+                        <?php $page->post_content ?>
+                    </div>
                 </div>
-            </div>
 
-        </article>
+            </article>
+        <?php endforeach; ?>
+
     </div>
 </section>
 
