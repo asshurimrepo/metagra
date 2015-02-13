f6bd98cfc49d2ef982eb2ab774c75005b95e3cc2
Admin <powerlogic1992@gmail.com>
2015-02-13 17:11:50 +0800

    contact page edited.

--- functions.php
+++ functions.php
@@ -5,6 +5,24 @@
 	require_once( 'inc/dimox_breadcrumbs.php' );
 	require_once( 'inc/posts_action.php' );
 
+	/**
+	 * Register our sidebars and widgetized areas.
+	 *
+	 */
+	function arphabet_widgets_init() {
+
+		register_sidebar( array(
+			'name'          => 'Home right sidebar',
+			'id'            => 'home_right_1',
+			'before_widget' => '<div>',
+			'after_widget'  => '</div>',
+			'before_title'  => '<h2 class="rounded">',
+			'after_title'   => '</h2>',
+		) );
+
+	}
+	add_action( 'widgets_init', 'arphabet_widgets_init' );
+
 	add_theme_support( 'menus' );
 
 	add_theme_support( 'post-thumbnails', array( 'post', 'page' ) );
