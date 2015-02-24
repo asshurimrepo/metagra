0d561494d71fd05aad9e5735f7955b75dd2a0369
Admin <powerlogic1992@gmail.com>
2015-02-24 09:03:21 +0800

    live exchanger  added.

--- my-widgets/widget-commodities.php
+++ my-widgets/widget-commodities.php
@@ -1,1 +1,8 @@
-asdasdsd
+<div class="widget-commodities">
+    <ul class="list-inline">
+        <li>Crude Oil <span><b style="color:#008800;">0.13</b></span><span><b style="color:#008800;"> (0.26%)</b></span></li>
+        <li>Crude Oil <span><b style="color:#008800;">0.13</b></span><span><b style="color:#008800;"> (0.26%)</b></span></li>
+        <li>Crude Oil <span><b style="color:#008800;">0.13</b></span><span><b style="color:#008800;"> (0.26%)</b></span></li>
+        <li>Crude Oil <span><b style="color:#008800;">0.13</b></span><span><b style="color:#008800;"> (0.26%)</b></span></li>
+    </ul>
+</div>
