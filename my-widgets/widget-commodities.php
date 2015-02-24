fae4a064092b1d4830eba417f39db3c38e5008f5
Admin <powerlogic1992@gmail.com>
2015-02-24 14:46:03 +0800

    live exchanger  added.

--- my-widgets/widget-commodities.php
+++ my-widgets/widget-commodities.php
@@ -34,8 +34,13 @@
     .ticker.energy{
         background-color: #f1f1f1;
     }
+    .ticker-group{
+        border: 2px solid #ccc;
+        margin-top: 30px;
+    }
 </style>
-<div class="ticker metal">
+<div class="ticker-group">
+    <div class="ticker metal">
     <span>
         <span class="desc">Copper </span> <span class="down-stock"><i class="fa fa-arrow-down"></i> 0.01 (0.47%)</span>
         <span class="desc">Gold 100 oz. </span> <span class="up-stock"><i class="fa fa-arrow-up"></i> 2.30 (0.17%)</span>
@@ -44,12 +49,13 @@
         <span class="desc">Silver 5000 oz.   </span> <span class="down-stock"><i class="fa fa-arrow-down"></i> 0.11 (0.51%)</span>
         <span class="desc">Silver </span> <span class="up-stock"><i class="fa fa-arrow-up"></i> 0.08 (0.48%)</span>
     </span>
-</div>
-<div class="ticker energy">
+    </div>
+    <div class="ticker energy">
     <span>
         <span class="desc">Crude Oil </span> <span class="up-stock"><i class="fa fa-arrow-up"></i> 0.08 (0.16%)</span>
         <span class="desc">Heating Oil</span> <span class="down-stock"><i class="fa fa-arrow-down"></i> 0.01 (0.51%)</span>
         <span class="desc">Natural Gas  </span> <span class="up-stock"><i class="fa fa-arrow-up"></i> 0.01 (0.24%)</span>
         <span class="desc">RBOB Gasoline </span> <span class="up-stock"><i class="fa fa-arrow-up"></i> 0.01 (0.24%)</span>
     </span>
+    </div>
 </div>
