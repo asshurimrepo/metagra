6d60527745a3bac1e4230e42e5f2aa249d5511f2
Admin <powerlogic1992@gmail.com>
2015-02-24 10:00:59 +0800

    live exchanger  added.

--- my-widgets/widget-commodities.php
+++ my-widgets/widget-commodities.php
@@ -19,10 +19,23 @@
         padding: 7px;
         font-size: 12px;
     }
-    * { font-family:'roboto Condensed', sans-serif}
+    .ticker .desc { margin: 0 5px; }
+    .ticker .up-stock {
+        color:#008800;
+        font-weight: bold;
+    }
+    .ticker .down-stock {
+        color:#cc0000;
+        font-weight: bold;
+    }
 </style>
 <div class="ticker">
     <span>
-        Crude Oil <span><b style="color:#008800;"><i class="fa fa-arrow-up"></i> 0.13</b></span><span><b style="color:#008800;"> (0.26%)</b></span>
+        <span class="desc">Copper </span> <span class="down-stock"><i class="fa fa-arrow-down"></i> 0.01 (0.47%)</span>
+        <span class="desc">Gold 100 oz. </span> <span class="up-stock"><i class="fa fa-arrow-up"></i> 2.30(0.17%)</span>
+        <span class="desc">Gold  </span> <span class="up-stock"><i class="fa fa-arrow-up"></i> 3.20 (0.27%)</span>
+        <span class="desc">Palladium   </span> <span class="up-stock"><i class="fa fa-arrow-up"></i> 0.65 (0.08%)</span>
+        <span class="desc">Silver 5000 oz.   </span> <span class="down-stock"><i class="fa fa-arrow-down"></i> 0.11 (0.51%)</span>
+        <span class="desc">Silver </span> <span class="up-stock"><i class="fa fa-arrow-up"></i> 0.08 (0.48%)</span>
     </span>
 </div>
