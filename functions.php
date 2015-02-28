734759835d6f3b4e735b781d8c284802a9e00a50
Admin <powerlogic1992@gmail.com>
2015-02-28 22:21:20 +0800

    live exchanger  added.

--- functions.php
+++ functions.php
@@ -4,6 +4,7 @@
 	require_once( 'inc/hooks.php' );
 	require_once( 'inc/dimox_breadcrumbs.php' );
 	require_once( 'inc/posts_action.php' );
+	require_once( 'inc/yahoo_finance.php' );
 
 	/**
 	 * Register our sidebars and widgetized areas.
