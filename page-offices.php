1b8ff7940bcd6f6bedf4b8461c008d4e5c8a0101
Admin <powerlogic1992@gmail.com>
2015-06-14 15:57:36 +0800

    added new addresses.

--- page-offices.php
+++ page-offices.php
@@ -11,6 +11,7 @@
 	<div class="box">
 		<h2 class="no-background void-top full-width"><span>Office in <b>Ukraine</b></span></h2>
 
+		<!--Location 1-->
 		<section class="map">
 			<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2723.4205444988474!2d32.0404875!3d46.9534267!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40c5cbbac7a49b05%3A0xcb7dc46a404802a0!2sMyru+Ave%2C+4%2C+Mykolaiv%2C+Mykolaivs&#39;ka+oblast%2C+Ukraine!5e0!3m2!1sen!2sph!4v1420962639106" width="100%" height="250" frameborder="0" style="border:0"></iframe></iframe>
 		</section>
@@ -19,7 +20,6 @@
 
 		<div class="info">
 			<div class="row">
-
 				<div class="col-md-4">
 					<div class="col-md-3 text-center"><img src="<?=asset('images/tracker-logo.png')?>" alt="Street Address"/></div>
 					<div class="col-md-9">
@@ -39,6 +39,65 @@
 				</div>
 			</div>
 		</div>
+
+
+		<!--Location 2 -->
+		<section class="map">
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12595.303562801098!2d58.35704685!3d37.88775425!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3f7002894af9c9fd%3A0x3b85fc7c01f89619!2sMyhmanhanalar+toplumy%2C+A%C5%9Fgabat%2C+Turkmenistan!5e0!3m2!1sen!2sph!4v1434268358981" width="100%" height="250" frameborder="0" style="border:0"></iframe>
+		</section>
+
+		<div class="space-md"></div>
+
+		<div class="info">
+			<div class="row">
+				<div class="col-md-4">
+					<div class="col-md-3 text-center"><img src="<?=asset('images/tracker-logo.png')?>" alt="Street Address"/></div>
+					<div class="col-md-9">
+						<h4>Street Address</h4>
+						<p>Office 401, Mizan Hotel, Archabil Avenue, Ashgabat, Turkmenistan</p>
+					</div>
+				</div>
+
+				<div class="col-md-4">
+					<div class="col-md-3 text-center"><img src="<?=asset('images/phone-icon.png')?>" alt="Phone"/></div>
+					<div class="col-md-9">
+						<h4>Phone</h4>
+						<p>Tel: +993 12 480676 <br/>
+							Fax: +993 12 488614</p>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+		<!--Location 3 -->
+		<section class="map">
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2995.6193332518933!2d69.28533159999999!3d41.338890299999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38ae8b53a7ec3c8d%3A0x186a1db9c8b975a4!2z0JDQvNC40YAg0KLQtdC80YPRgCDRiNC-0rMg0LrRntGH0LDRgdC4LCDQotC-0YjQutC10L3RgiwgVXpiZWtpc3Rhbg!5e0!3m2!1sen!2sph!4v1434268564902" width="100%" height="250" frameborder="0" style="border:0"></iframe>
+		</section>
+
+		<div class="space-md"></div>
+
+		<div class="info">
+			<div class="row">
+				<div class="col-md-4">
+					<div class="col-md-3 text-center"><img src="<?=asset('images/tracker-logo.png')?>" alt="Street Address"/></div>
+					<div class="col-md-9">
+						<h4>Street Address</h4>
+						<p>Office 401, Mizan Hotel, Archabil Avenue, Ashgabat, Turkmenistan</p>
+					</div>
+				</div>
+
+				<div class="col-md-4">
+					<div class="col-md-3 text-center"><img src="<?=asset('images/phone-icon.png')?>" alt="Phone"/></div>
+					<div class="col-md-9">
+						<h4>Phone</h4>
+						<p>Tel: +993 12 480676 <br/>
+							Fax: +993 12 488614</p>
+					</div>
+				</div>
+			</div>
+		</div>
+
 
 	</div>
 </div>
