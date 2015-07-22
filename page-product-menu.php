cc7576ff30fcf46a552588bb3ea8c1146646ef40
Admin <powerlogic1992@gmail.com>
2015-07-22 20:37:34 +0800

    added fertilizer image

--- page-product-menu.php
+++ page-product-menu.php
@@ -7,7 +7,7 @@ Author: Juni Brosas @ iBoostme
 get_header();
 $args = [
     'parent' => 8,
-    'sort_order' => 'desc',
+    'sort_order' => 'asc',
     'sort_column' => 'menu_order',
 ];
 $pages = get_pages( $args );
